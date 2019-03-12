748118985d82327980558baac3ec9dbe39eb770d
Hasanuzzaman333 <30312880+Hasanuzzaman333@users.noreply.github.com>
2019-03-12 12:02:26 +0600

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Hello World!");
+            Console.WriteLine("Hello World! Hello World! hello world again");
         }
     }
 }
